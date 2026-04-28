e9d743b8ca9dd12a8068178999c4729579cee241
Abel <work@owenabel.com>
2026-04-28 10:43:38 +0100

    Expand group invite link classifier

--- src/moderation/groupInviteLink.ts
+++ src/moderation/groupInviteLink.ts
@@ -21,7 +21,7 @@ const HIGH_PRECISION_PATTERNS: Array<{ label: string; regex: RegExp }> = [
   },
   {
     label: "link for chat/group",
-    regex: /\blink\s+(?:for|to|2)\s+(?:the\s+)?(?:chat|group|whatsapp|watsapp|wa|channel|community)\b/iu,
+    regex: /\blink\s+(?:for|to|2)\s+(?:(?:the|this|that|our|current)\s+){0,2}(?:chat|group|whatsapp|watsapp|wa|channel|community)\b/iu,
   },
   {
     label: "join link",
@@ -36,14 +36,29 @@ const HIGH_PRECISION_PATTERNS: Array<{ label: string; regex: RegExp }> = [
     regex: /\b(?:group|chat|whatsapp|watsapp|wa|channel|community)\s+invite\b/iu,
   },
   {
+    label: "link to send someone to join group",
+    regex:
+      /\blink\b(?=.{0,80}\b(?:send|share|forward|give|pass)\b)(?=.{0,120}\b(?:someone|somebody|some1|whoever|a\s+friend|my\s+friend|friends|my\s+friends|a\s+mate|my\s+mate|mates|my\s+mates|people|person|them|him|her)\b)(?=.{0,120}\b(?:join|get\s+into|get\s+in|be\s+added|add(?:ed)?)\b)(?=.{0,140}\b(?:(?:the|this|that|our|current)\s+){0,2}(?:group|chat|whatsapp|watsapp|wa|channel|community)\b)/iu,
+  },
+  {
+    label: "shareable link for someone to join group",
+    regex:
+      /\blink\b(?=.{0,120}\b(?:someone|somebody|some1|whoever|a\s+friend|my\s+friend|friends|my\s+friends|a\s+mate|my\s+mate|mates|my\s+mates|people|person|them|him|her)\b)(?=.{0,120}\b(?:can|could)?\s*(?:use\s+)?(?:to\s+)?(?:join|get\s+into|get\s+in|be\s+added)\b)(?=.{0,140}\b(?:(?:the|this|that|our|current)\s+){0,2}(?:group|chat|whatsapp|watsapp|wa|channel|community)\b)/iu,
+  },
+  {
+    label: "link to add/invite someone to group",
+    regex:
+      /\blink\b(?=.{0,120}\b(?:add|invite)\b)(?=.{0,120}\b(?:someone|somebody|some1|whoever|a\s+friend|my\s+friend|friends|my\s+friends|a\s+mate|my\s+mate|mates|my\s+mates|people|person|them|him|her)\b)(?=.{0,140}\b(?:to|into|2)\s+(?:(?:the|this|that|our|current)\s+){0,2}(?:group|chat|whatsapp|watsapp|wa|channel|community)\b)/iu,
+  },
+  {
     label: "invite/add someone to group",
     regex:
-      /\b(?:how\s+(?:can|do)\s+i\s+)?(?:invite(?:\s+add)?|add)\s+(?:me|someone|somebody|some1|a\s+friend|my\s+friend|a\s+mate|my\s+mate|people|person|them|him|her)\s+(?:to|into|2)\s+(?:the\s+)?(?:group|chat|whatsapp|watsapp|wa|channel|community)\b/iu,
+      /\b(?:how\s+(?:can|do)\s+i\s+)?(?:invite(?:\s+add)?|add)\s+(?:me|someone|somebody|some1|whoever|a\s+friend|my\s+friend|friends|my\s+friends|a\s+mate|my\s+mate|mates|my\s+mates|people|person|them|him|her)\s+(?:to|into|2)\s+(?:(?:the|this|that|our|current)\s+){0,2}(?:group|chat|whatsapp|watsapp|wa|channel|community)\b/iu,
   },
   {
     label: "how to join group",
     regex:
-      /\bhow\s+(?:can|do)\s+i\s+(?:join|get\s+into|get\s+in)\s+(?:the\s+)?(?:group|chat|whatsapp|watsapp|wa|channel|community)\b/iu,
+      /\bhow\s+(?:can|do)\s+i\s+(?:join|get\s+into|get\s+in)\s+(?:(?:the|this|that|our|current)\s+){0,2}(?:group|chat|whatsapp|watsapp|wa|channel|community)\b/iu,
   },
 ];
 
